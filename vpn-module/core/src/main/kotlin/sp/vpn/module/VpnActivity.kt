7b62e76b228e831b68d67ac180f885db74d3809f
MehraB832 <mehrabshafae@gmail.com>
2024-05-26 12:41:03 +0330

    update

--- vpn-module/core/src/main/kotlin/sp/vpn/module/VpnActivity.kt
+++ vpn-module/core/src/main/kotlin/sp/vpn/module/VpnActivity.kt
@@ -2,11 +2,11 @@ package sp.vpn.module
 
 abstract class VpnActivity : sp.openconnect.remote.CiscoMainActivity() {
     // default cisco options
-    override fun isEnableDialog(): Boolean {
+    override fun CiscoIsEnableDialog(): Boolean {
         return false // skip dialog (cisco)
     }
 
-    override fun skipCertWarning(): Boolean {
+    override fun CiscoSkipCertWarning(): Boolean {
         return true // skip certification warning
     }
 }
